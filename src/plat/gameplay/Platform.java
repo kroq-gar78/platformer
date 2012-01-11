718211ee80797cc23663b5ef393e7d0e92bcd9c3
Aditya Vaidya <kroq.gar78@gmail.com>
2012-01-10 18:45:12 -0600

    Added some stuff to the platform class.

--- src/plat/gameplay/Platform.java
+++ src/plat/gameplay/Platform.java
@@ -1,22 +1,44 @@
 package plat.gameplay;
 
+import net.phys2d.raw.StaticBody;
+import net.phys2d.math.Vector2f;
+import net.phys2d.raw.shapes.Shape;
+
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.geom.Shape;
-import org.newdawn.slick.geom.Vector2f;
 
 
-public class Platform extends CollidableImageObject
+public class Platform extends StaticBody
 {
 
-	public Platform(String name, Image image, Vector2f position, Shape collisionShape, int collisionType)
+	/*public Platform(String name, Image image, Vector2f position, Shape collisionShape, int collisionType)
 	{
 		super(name, image, position, collisionShape, collisionType);
 		// TODO Auto-generated constructor stub
-	}
+	}*/
 	
+	public Platform(Shape shape,Vector2f pos,Image img)
+	{
+		super(shape);
+		this.img=img;
+	}
+	public Platform(String name,Shape shape,Vector2f pos,Image img)
+	{
+		super(name,shape);
+		this.setPosition(pos.x,pos.y);
+		this.img=img;
+	}
+	public Platform(String name,Shape shape,float x,float y,Image img)
+	{
+		super(name,shape);
+		this.setPosition(x,y);
+		this.img=img;
+	}
+
 	public void render(Graphics g)
 	{
-		super.render(g);
+		img.draw(this.getPosition().getX(),this.getPosition().getY());
 	}
+	
+	private Image img;
 }
