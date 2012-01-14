88eb94f694e98f1d70a5913b1e913111bd341c9e
Aditya Vaidya <kroq.gar78@gmail.com>
2012-01-14 00:08:13 -0600

    Shrunk player image to 1/2 size, and made the world iterate at 3/4 max FPS

--- src/plat/gameplay/GameplayState.java
+++ src/plat/gameplay/GameplayState.java
@@ -34,7 +34,7 @@ public class GameplayState extends BasicGameState
 		platformImage = new Image("res/platform.png");
 		
 		soundWrapper = new SoundWrapper();
-		worldIter=PlatformerGame.MAX_FPS*2/4;
+		worldIter=PlatformerGame.MAX_FPS*3/4;
 		world = new World(new Vector2f(0f,1.5f), worldIter);
 		map = new TiledMapPlus("res/map.tmx");
 		mapUtil = new MapUtil(map, world);
