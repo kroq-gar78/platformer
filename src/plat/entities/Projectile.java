776cea0802f1cc66b91b5f6627a4ae75be69fb72
Aditya Vaidya <kroq.gar78@gmail.com>
2012-02-04 15:47:29 -0600

    Commented out the old Animation settings for now.

--- src/plat/entities/Projectile.java
+++ src/plat/entities/Projectile.java
@@ -10,24 +10,25 @@ import plat.AnimationEntity;
 
 import net.phys2d.raw.World;
 
+import com.n3wt0n.G2DP.Entity;
 import com.n3wt0n.G2DP.SoundWrapper;
 
-public class Projectile extends AnimationEntity
+public class Projectile extends Entity
 {
-	public Projectile(World world, float x, float y, float mass, String name, SoundWrapper soundWrapper, Animation anim)
+	public Projectile(World world, float x, float y, float mass, String name, SoundWrapper soundWrapper, Image image)
 	{
-		super(world, x, y, anim.getWidth(), anim.getHeight(), mass, name, soundWrapper);
-		setAnimation(anim);
+		super(world, x, y, image.getWidth(), image.getHeight(), mass, name, soundWrapper);
+		setImage(image);
 	}
 	
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		//getImage().draw(this.getX(), this.getY());
-		super.render(gc, g);
+		getImage().draw(this.getX(), this.getY());
+		//super.render(gc, g);
 	}
 	
-	public void update(GameContainer gc, int delta)
+	public void update(GameContainer gc, int delta) throws SlickException
 	{
-		super.update(gc,delta);
+		super.update(delta);
 	}
 }
