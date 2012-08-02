2ee1f469508febafc63c87d9013dcbe9de6d5038
Aditya Vaidya <kroq.gar78@gmail.com>
2012-08-02 01:11:49 -0500

    Fixed fizzy.examples.SimpleRenderTest.

--- src/org/newdawn/fizzy/examples/SimpleRenderTest.java
+++ src/org/newdawn/fizzy/examples/SimpleRenderTest.java
@@ -1,8 +1,9 @@
 package org.newdawn.fizzy.examples;
 
+import org.jbox2d.collision.shapes.CircleShape;
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
 import org.newdawn.fizzy.Body;
-import org.newdawn.fizzy.Circle;
-import org.newdawn.fizzy.Rectangle;
 import org.newdawn.fizzy.World;
 
 /**
@@ -23,13 +24,17 @@ public class SimpleRenderTest extends AbstractTest {
 
 	@Override
 	public World createWorld() {
-		World world = new World(800,800);
+		World world = new World(new Vec2(800,800));
 		
 		for (int i=0;i<5;i++) {
-			Body body = new Body(new Circle(10.0f), i, 20*i);
+			CircleShape cs = new CircleShape();
+			cs.m_radius = 10.0f;
+			Body body = new Body(cs, i, 20*i);
 			world.add(body);
 		}
-		Body floor = new Body(new Rectangle(200.0f, 10.0f), 0, -50.0f, true);
+		PolygonShape floorShape = new PolygonShape();
+		floorShape.setAsBox(100.0f, 10.0f);
+		Body floor = new Body(floorShape, 0, -50.0f, true);
 		world.add(floor);
 		
 		return world;
